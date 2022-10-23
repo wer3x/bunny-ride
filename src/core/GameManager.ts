d5a351caa280695c179db4c4d3341db6e2653d01
Eugenii Petrukhin <ep@MacBook-Air-Eugenii.local>
2022-10-23 17:11:30 +0300

    Added fonts, sounds, autosize canvas

--- src/core/GameManager.ts
+++ src/core/GameManager.ts
@@ -1,43 +1,92 @@
+// PIXI.Loader.registerPlugin(PIXI.WebfontLoaderPlugin);
 import * as PIXI from 'pixi.js';
+import {Howl} from 'howler';
 import {LoaderResource} from "pixi.js";
 import {Loader} from "@pixi/loaders";
 import {Dict} from "@pixi/utils";
+import {WebfontLoaderPlugin} from "pixi-webfont-loader";
 // Run the game
 export class GameManager{
-    private _app: PIXI.Application;
+    private _app!: PIXI.Application;
+    private _embient!: Howl;
 
     public constructor(){
-        this._app = new PIXI.Application({backgroundColor: 0xFFFFFF });
-        document.body.appendChild(this._app.view);
+        const root: HTMLElement | null = document.querySelector('#root');
+        if (!root) {
+            return;
+        }
+        this._setupSounds();
+        Loader.registerPlugin(WebfontLoaderPlugin);
+        // console.warn('devicePixelRatio: ', devicePixelRatio);
+        this._app = new PIXI.Application({
+            resizeTo: window,
+            backgroundColor: PIXI.utils.string2hex('#5098C8'),
+            // resolution: devicePixelRatio
+        });
+        root?.appendChild(this._app.view);
 
-        this._app.loader.add('bunnyRider', 'assets/mi_bunny_idle_03.png');
+        this._app.loader
+            .add('zubiloBlack', 'assets/fonts/Zubilo Black.otf')
+            .add('bunnyRider', 'assets/mi_bunny_idle_03.png')
+            .add('btnFullScreen', 'assets/ui/btn_fullscreen_active.png')
+            .add('btnFullScreenHover', 'assets/ui/btn_fullscreen_hover.png')
+            .add('btnFullScreenPressed', 'assets/ui/btn_fullscreen_press.png')
+            .add('collectCoinIcon', 'assets/ui/collect_coin_icon.png')
+            .add('coinScorePlate', 'assets/ui/coin_score_plate.png');
         this._app.loader.load((loader: Loader,
-                               resources: Dict<LoaderResource>) => this.onCompleteSignal(loader, resources));
+                               resources: Dict<LoaderResource>) => this._onCompleteSignal(loader, resources));
     }
 
-    private onCompleteSignal(
+    private _onCompleteSignal(
         loader: Loader,
         resources: Dict<LoaderResource>
     ): void{
         console.warn('res: ', resources);
         //create a sprite from a 'gecko.png' image
-        const gecko:PIXI.Sprite = new PIXI.Sprite(resources?.bunnyRider?.texture);
+        const bunny:PIXI.Sprite = new PIXI.Sprite(resources?.bunnyRider?.texture);
+
 
         //position the gecko in the center of the screen
-        gecko.x = this._app.renderer.width / 2;
-        gecko.y = this._app.renderer.height / 2;
+        bunny.scale.set(0.5);
+        bunny.x = this._app.renderer.width / 2;
+        bunny.y = this._app.renderer.height / 2;
 
         //add an anchor so the rotate pivots the center of the image
-        gecko.anchor.x = 0.5;
-        gecko.anchor.y = 0.5;
+        bunny.anchor.set(0.5, 0.5);
 
         //add the gecko to the screen
-        this._app.stage.addChild(gecko);
+        this._app.stage.addChild(bunny);
+        const skewText = new PIXI.Text(
+            'Bunny Rider',
+            {
+                fontFamily: 'zubiloBlack',
+                fontSize: 32,
+                fill: '#003d71'
+            }
+        );
+        skewText.x = this._app.renderer.width / 2;
+        skewText.y = this._app.renderer.height / 10;
+            // .skew.set(0.65, -0.3);
+        skewText.anchor.set(0.5, 0.5);
+        this._app.stage.addChild(skewText);
 
         //listen for frame updates
         this._app.ticker.add(() => {
             //each frame spin the gecko around a tiny bit
-            gecko.rotation -= 0.01;
+            bunny.rotation -= 0.01;
         });
+    }
+
+
+    private _setupSounds(): void {
+        this._embient = new Howl({
+            src: ['assets/sounds/music.mp3'],
+            autoplay: true,
+            loop: true,
+            volume: 0.5,
+        });
+        // this.hitSound = new Howl({
+        //     src: [this.assetBase + 'hit2.wav']
+        // });
     }
 }
